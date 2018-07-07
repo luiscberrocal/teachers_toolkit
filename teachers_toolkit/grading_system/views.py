c1bf162d637062ff947d22a501a4f0690113fa1e
Unknown <luis.berrocal.1942@gmail.com>
2018-07-07 18:28:14 -0500

    Stopped creating the assingments

--- teachers_toolkit/grading_system/views.py
+++ teachers_toolkit/grading_system/views.py
@@ -8,6 +8,7 @@ from django.views.generic import TemplateView, ListView
 
 from teachers_toolkit.grading_system.models import Assignment, Student, AssignmentResult
 
+
 class AssignmentListView(ListView):
     model = Assignment
     context_object_name = 'assignments'
@@ -19,15 +20,13 @@ class AssignmentListView(ListView):
         return qs
 
 
-
-
 class GradeAssingmentView(TemplateView):
     template_name = 'grading_system/grading_assingment.html'
 
     def get_context_data(self, **kwargs):
         ctx = super(GradeAssingmentView, self).get_context_data(**kwargs)
         ctx['assignment'] = Assignment.objects.get(pk=self.kwargs['pk'])
-        if self.kwargs.get('create'):
+        if  self.kwargs.get('create'):
             students = Student.objects.all()
             assignment_results = list()
             for student in students:
@@ -40,11 +39,17 @@ class GradeAssingmentView(TemplateView):
                     assignment_results.append(result)
             ctx['results'] = assignment_results
         else:
-            ctx['results'] = AssignmentResult.objects.filter(assignment=ctx['assignment'])
+            if kwargs.get('filter') == 'not-received':
+                ctx['results'] = AssignmentResult.objects.filter(
+                    assignment=ctx['assignment'],
+                    grade=Decimal('0.0')).order_by('student__last_name', 'student__first_name')
+            else:
+                ctx['results'] = AssignmentResult.objects.filter(
+                    assignment=ctx['assignment']).order_by('student__last_name', 'student__first_name')
         return ctx
 
     def post(self, request, *args, **kwargs):
-        #assignment = Assignment.objects.get(pk=self.kwargs['pk'])
+        # assignment = Assignment.objects.get(pk=self.kwargs['pk'])
         regexp = re.compile(r'^grade_student_(\d+)$')
         for key, value in request.POST.items():
             match = regexp.match(key)
@@ -52,7 +57,7 @@ class GradeAssingmentView(TemplateView):
                 student_pk = int(match.group(1))
                 comment_key = 'comment_student_{}'.format(student_pk)
                 assignment_result_pk_key = 'assignment_result_pk_{}'.format(student_pk)
-                #student = Student.objects.get(pk=student_pk)
+                # student = Student.objects.get(pk=student_pk)
                 comment = request.POST[comment_key]
                 assingment_result_pk = int(request.POST[assignment_result_pk_key])
                 result = AssignmentResult.objects.get(id=assingment_result_pk)
